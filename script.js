8d7033a73669d843e953877cd972757dda955182
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-16 10:45:41 +0530

    almost complete

---
+++ script.js
@@ -0,0 +1,18 @@
+const todo = document.querySelector("#todo");
+const Progress = document.querySelector("#Progress");
+const done = document.querySelector("#done");
+
+const tasks = document.querySelectorAll(".task");
+
+// console.log(todo);
+
+tasks.forEach(task => {
+    task.addEventListener("drag",(e) => {
+
+    })
+})
+
+function addDragEventsOnColoumn
+addDragEventsOnColoumn(todo);
+addDragEventsOnColoumn(Progress);
+addDragEventsOnColoumn(done);
